2f972a43b703304a5f0b0f30d43e3ada2b76a777
willc <will7101c@gmail.com>
2018-11-06 17:02:05 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ setup(
     version='0.2',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=[
+        'opencv-python',
+        'numpy',
+    ],
 
     author='willC',
     author_email='will7101c@gmail.com',
